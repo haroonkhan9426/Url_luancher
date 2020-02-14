c3092b70f56a10bbbbe5bafd5a45b8d7749a8a88
Haroon khan <haroonkhan9426@gmail.com>
2020-02-15 00:22:19 +0500

    Image assets added

--- lib/splash-screen.dart
+++ lib/splash-screen.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Image.asset(''),
+    );
   }
 }
